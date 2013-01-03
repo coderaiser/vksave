52a82e3e59d34d60fb4aa08f6ffe54b147096134
coderaiser <mnemonic.enemy@gmail.com>
2013-01-03 13:00:32 +0200

    Update vksave.js

--- vksave.js
+++ vksave.js
@@ -1,6 +1,10 @@
 (function(document){
     'use strict';
             
+    function getById(pId, pElement){
+        return (pElement || document).getElementById(pId);
+    }
+    
     function getByClass(pClass, pElement){
         return (pElement || document).getElementsByClassName(pClass);
     }
@@ -33,26 +37,30 @@
     var create      = document.createElement.bind(document),
         area        = getByClass('area');
         
-        for (var i=0, n=area.length; i < n ; i++){
-            var input   = getByTag('input', area[i]) [0],
-                title   = getByClass('title', area[i])[0],
+        for (var i = 0, n = area.length; i < n ; i++){
+            var id = 'vksave_' + i;
+            if( !getById(id) ){
+                var input   = getByTag('input', area[i]) [0],
+                    title   = getByClass('title', area[i])[0],
+                    
+                    artist  = getByClass('title_wrap',  area[i]) [0].textContent,
+                    name    = title.textContent,              
+                    
+                    link    = create('a'),
+                    src     = input.value,
+                    coma    = src.indexOf(','),
+                    replace = src.substr(coma);
+                    
+                src = src.replace(replace, '');
                 
-                artist  = getByClass('title_wrap',  area[i]) [0].textContent,
-                name    = title.textContent,              
+                link.href           = src;
+                link.textContent    = getLinkName();
+                link.download       = artist + name + '.mp3';
+                link.id             = id;
                 
-                link    = create('a'),
-                src     = input.value,
-                coma    = src.indexOf(','),
-                replace = src.substr(coma);
-                
-            src = src.replace(replace, '');
-            
-            link.href           = src;
-            link.textContent    = getLinkName();
-            link.download       = artist + name + '.mp3';
-            
-            title.appendChild(create('br'));
-            title.appendChild(link);
+                title.appendChild(create('br'));
+                title.appendChild(link);
+            }
         }
     
 })(document);
